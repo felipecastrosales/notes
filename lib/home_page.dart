a1a691f1194306aa06454fbb6072b2c1a90ecbc6
Felipe Sales <[soufeliposales@gmail.com]>
2021-06-14 10:34:46 -0300

    Adding the Scaffold

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.purple
-    );
+    return Scaffold();
   }
 }

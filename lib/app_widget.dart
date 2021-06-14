da5474a06cfe81ee69594177aa33e75e7f781713
Felipe Sales <[soufeliposales@gmail.com]>
2021-06-14 10:20:48 -0300

    Creating the HomePage

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Notes',
-      home: Container(
-        color: Colors.purple
-      ),
+      home: HomePage(),
     );
   }
 }

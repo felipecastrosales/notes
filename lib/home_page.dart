0bb00b6031d688e189166c4e0169249111de65d8
Felipe Sales <[soufeliposales@gmail.com]>
2021-06-14 10:45:09 -0300

    Adding the AppBar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('NOTES'),
+        centerTitle: true,
+      ),
+    );
   }
 }

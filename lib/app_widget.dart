477a4e61b0d5f5624aa9794739b8407a5dd8c344
Felipe Sales <[soufeliposales@gmail.com]>
2021-06-14 12:50:25 -0300

    Adding the Navigator between pages

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-import 'create_note_page.dart';
+import 'home_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Notes',
       debugShowCheckedModeBanner: false,
-      home: CreateNotePage(),
+      title: 'Notes',
+      home: HomePage(),
     );
   }
 }

f24907620318aeb5d2d28da2fe87ac4bafd3b190
Felipe Sales <soufeliposales@gmail.com>
2024-06-30 09:31:19 -0300

    feat!: update project to latest flutter versions and enhancements

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -4,6 +4,8 @@ import 'create_note_page.dart';
 import 'home_page.dart';
 
 class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,9 +14,9 @@ class AppWidget extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.purple),
       initialRoute: '/home',
       routes: {
-        '/home': (context) => HomePage(),
-        '/create-note': (context) => CreateNotePage(),
-      }
+        '/home': (context) => const HomePage(),
+        '/create-note': (context) => const CreateNotePage(),
+      },
     );
   }
 }
